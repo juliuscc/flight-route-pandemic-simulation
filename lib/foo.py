ebac458f5a030b38f3b02f56829740fa637c91aa
jcelik <julius.cc@hotmail.com>
2019-04-29 13:44:57 +0200

    Started using networkx

--- lib/foo.py
+++ lib/foo.py
@@ -1,5 +1,11 @@
 from read import read_routes_from_file
+import networkx as nx
 
 routes = read_routes_from_file("../input/routes.dat")
 
+G = nx.Graph()
+
+G.add_edges_from(routes)
+
 print(routes[0])
+print(G.number_of_nodes())
