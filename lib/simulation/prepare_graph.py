8a91c9f89ed1e82ada14c104de0faf2948553157
jcelik <julius.cc@hotmail.com>
2019-05-01 13:35:14 +0200

    Added prepare graph

--- lib/simulation/prepare_graph.py
+++ lib/simulation/prepare_graph.py
@@ -1,12 +1,6 @@
 def prepare_graph(graph):
 
-    print(graph.nodes.data())
-
-    print(list(graph.nodes))
-
     for node in list(graph.nodes):
-        # graph[node]['test'] = 'Monday'
-        print(node)
-    #     nodes['test'] = 'Monday'
+        graph.add_node(node, contaminated=False)
 
     return graph
--- lib/temp.py
+++ lib/temp.py
@@ -3,10 +3,12 @@ from simulation.prepare_graph import prepare_graph
 
 graph = nx.fast_gnp_random_graph(10, 0.2, None, True)
 
-# prepare_graph(graph)
+prepare_graph(graph)
 
-graph.nodes[0]['day'] = 'Monday'
-graph.nodes[1]['day'] = 'Tuesday'
+# graph.nodes[0]['day'] = 'Monday'
+# graph.nodes[1]['day'] = 'Tuesday'
+
+# graph.add_node(0, day='Monday')
 
 print(graph.nodes)
 print(graph.nodes.data())
