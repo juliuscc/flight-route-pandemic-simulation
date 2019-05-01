b2db6df21839fffa0974c87e187f4269a2edb1c0
Patric <pridell@kth.se>
2019-05-01 14:16:42 +0200

    added read_airports

--- lib/main.py
+++ lib/main.py
@@ -1,8 +1,10 @@
 from read import read_routes_from_file
+from read_airports import read_airports_from_file
 from betweenness import get_betweenness_value
 import networkx as nx
 
 routes = read_routes_from_file("../input/routes.dat")
+airportDict = read_airports_from_file("../input/airports.dat") # Dictionary with {'id': (0 = airport name, 1 = city, 2 = country)}
 
 G = nx.DiGraph()
 G.add_edges_from(routes)
