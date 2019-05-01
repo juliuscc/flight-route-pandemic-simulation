7e9d73efaea67226bb47d8e2a5712b15ea05afe5
Unknown <julius.cc@hotmail.com>
2019-05-01 11:21:39 +0200

    Rename main file

--- lib/main.py
+++ lib/main.py
@@ -4,11 +4,9 @@ import networkx as nx
 
 routes = read_routes_from_file("../input/routes.dat")
 
-print(routes)
-
 G = nx.DiGraph()
 G.add_edges_from(routes)
 
 sorted_list = get_betweenness_value(G)
 
-print(sorted_list)
+print(sorted_list[0])
