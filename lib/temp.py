b13a1956805ace701f6d7a0fc40b39bcf7f393a8
andreas <rosenba@kth.se>
2019-05-01 15:11:53 +0200

    Added check progress

--- lib/temp.py
+++ lib/temp.py
@@ -1,6 +1,8 @@
 import networkx as nx
 from simulation.prepare_graph import prepare_graph
 from simulation.iterate import iterate_graph
+from simulation.check_progress import check_progress
+
 
 graph = nx.fast_gnp_random_graph(5, 0.4, 1)
 
@@ -8,14 +10,18 @@ prepare_graph(graph)
 
 graph.add_node(0, contaminated=True, contaminated_step=0)
 
-print(graph.edges)
+print("Printing edges", graph.edges)
 print(graph.nodes.data())
+
+check_progress(graph)
 
 iterate_graph(graph)
 print(graph.nodes.data())
 
+check_progress(graph)
 iterate_graph(graph)
 print(graph.nodes.data())
 
+check_progress(graph)
 iterate_graph(graph)
 print(graph.nodes.data())
