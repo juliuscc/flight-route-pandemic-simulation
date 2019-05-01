ae856e06fe08b3233d6b6e32313065999d20f1d4
jcelik <julius.cc@hotmail.com>
2019-05-01 13:26:00 +0200

    Started on simulation

---
+++ lib/simulation/prepare_graph.py
@@ -0,0 +1,12 @@
+def prepare_graph(graph):
+
+    print(graph.nodes.data())
+
+    print(list(graph.nodes))
+
+    for node in list(graph.nodes):
+        # graph[node]['test'] = 'Monday'
+        print(node)
+    #     nodes['test'] = 'Monday'
+
+    return graph
