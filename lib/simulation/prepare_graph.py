84a628e7b315317cb642add4317fdd237d090eb9
jcelik <julius.cc@hotmail.com>
2019-05-01 13:38:06 +0200

    Update prepare_graph with a counter

--- lib/simulation/prepare_graph.py
+++ lib/simulation/prepare_graph.py
@@ -1,6 +1,6 @@
 def prepare_graph(graph):
 
     for node in list(graph.nodes):
-        graph.add_node(node, contaminated=False)
+        graph.add_node(node, contaminated=False, contaminated_step=-1)
 
     return graph
