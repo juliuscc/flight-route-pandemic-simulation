43a9c0b1ac057206ec2a0463a661f4f26b0684f8
jcelik <julius.cc@hotmail.com>
2019-05-01 13:32:50 +0200

    Added temp file with examples

---
+++ lib/temp.py
@@ -0,0 +1,14 @@
+import networkx as nx
+from simulation.prepare_graph import prepare_graph
+
+graph = nx.fast_gnp_random_graph(10, 0.2, None, True)
+
+# prepare_graph(graph)
+
+graph.nodes[0]['day'] = 'Monday'
+graph.nodes[1]['day'] = 'Tuesday'
+
+print(graph.nodes)
+print(graph.nodes.data())
+print(graph.edges)
+print(graph.graph)
