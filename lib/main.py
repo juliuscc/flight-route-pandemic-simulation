ed0dcf6eff012e2aa5633548506c361427c95115
Unknown <julius.cc@hotmail.com>
2019-05-10 14:23:30 +0200

    Minor refactor

--- lib/main.py
+++ lib/main.py
@@ -1,30 +1,13 @@
 import os
 import networkx as nx
 from flow_of_passengers import read_graph_with_importance
-# from read import read_routes_from_file
-# from read_airports import read_airports_from_file
 # from betweenness import get_betweenness_value
 # from simulation.driver import test_all_nodes
 from statistics import mean
 
-# dirname = os.path.dirname(__file__)
-
-# routes_filename = os.path.join(dirname, '../input/routes.dat')
-# airports_filename = os.path.join(dirname, '../input/airports.dat')
-
-# routes = read_routes_from_file(routes_filename)
-
-# # Dictionary with {'id': (0 = airport name, 1 = city, 2 = country)}
-# airportDict = read_airports_from_file(airports_filename)
-
-# G = nx.Graph()
-# G.add_edges_from(routes)
-
 G = read_graph_with_importance()
 
 print(G.nodes.data())
-
-# print(G.nodes.data())
 
 # steps_for_all_nodes = test_all_nodes(G)
 # print(steps_for_all_nodes)
