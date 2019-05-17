b97875ea074d72f939e411abfc861deca5a417e3
Hannes Rabo <hannes.rabo@gmail.com>
2019-05-17 10:40:43 +0200

    Added new simulation script

--- lib/simulation/simulation_const.py
+++ lib/simulation/simulation_const.py
@@ -7,31 +7,25 @@ BETA_KEEP_LOCAL_STATE_FRACTION = 0.5
 
 # omega = 0
 SEIR_RATE_RECOVERY = 0
-
-# natural death 255 days
-SEIR_RATE_NATURAL_DEATH = 0
-
-# natural birth nu = mu
-SEIR_RATE_NATURAL_BIRTH = SEIR_RATE_NATURAL_DEATH
-
-# latent period 1.2 days sigma^-1
-SEIR_PERIOD_LATENT = 1 / 1.2
-
-# infected period gamma = sigma for the influenza
-SEIR_INFECTED_PERIOD = 1 / 1.2
-
-# transmission coefficient beta 1.66 days
-SEIR_TRANSMISSION_COEFFICIENT = 1 / 1.66
+SEIR_RATE_NATURAL_DEATH = 0     # mu
+SEIR_RATE_NATURAL_BIRTH = 0     # nu
 
 # h = 0.1
 SEIR_TIME_STEP = 0.5
 
-##################################
-# MEASLES
-##################################
-# SEIR_PERIOD_LATENT = 0.143
+# Influenza
+SEIR_PERIOD_LATENT = 1 / 1.2                # sigma
+SEIR_INFECTED_PERIOD = 1 / 1.2              # gamma
+SEIR_TRANSMISSION_COEFFICIENT = 1 / 1.66    # beta
+
 
 # TUBERCULOSIS
 SEIR_PERIOD_LATENT = 0.000310               # sigma
 SEIR_INFECTED_PERIOD = 0.123111             # gamma
 SEIR_TRANSMISSION_COEFFICIENT = 0.326655    # beta
+
+
+# Smallpox
+SEIR_PERIOD_LATENT = 3                      # sigma
+SEIR_INFECTED_PERIOD = 19/60                # gamma
+SEIR_TRANSMISSION_COEFFICIENT = 6/5         # beta
