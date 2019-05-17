d8e84084b7a11995e7da62043e2a5b64f9baec66
Hannes Rabo <hannes.rabo@gmail.com>
2019-05-17 15:11:12 +0200

    Final simulation runs with results done!

--- lib/main.py
+++ lib/main.py
@@ -44,11 +44,11 @@ def runForRanges(G, block_set, prefix=""):
 
 G = read_graph_w_extrapolated_flow()
 empty = set()
-runForRanges(G, empty, "base")
-exclude_eigen = eigen_vector_centrality.get_exclude_set(G, 20)
-runForRanges(G, exclude_eigen, "eigen")
-exclude_betweenness = betweenness.get_betweenness_exclude_set(G, 20)
-runForRanges(G, exclude_betweenness, "betweenness")
+# runForRanges(G, empty, "base")
+# exclude_eigen = eigen_vector_centrality.get_exclude_set(G, 20)
+# runForRanges(G, exclude_eigen, "eigen")
+# exclude_betweenness = betweenness.get_betweenness_exclude_set(G, 20)
+# runForRanges(G, exclude_betweenness, "betweenness")
 exclude_population = population_centrality.get_exclude_set(G, 20)
 runForRanges(G, exclude_population, "population")
 
@@ -61,15 +61,33 @@ runForRanges(G, exclude_population, "population")
 # plot "sim/base-r0-n0.dat" u 1:4 axes x1y1 w lines lw 2 title "Infected", "sim/base-r0-n0.dat" u 1:5 axes x1y1 w lines lw 2 title "Recovered", "sim/eigen-r0-n0.dat" u 1:4 axes x1y1 w lines lw 2 title "Infected - eigen", "sim/eigen-r0-n0.dat" u 1:5 axes x1y1 w lines lw 2 title "Recovered - eigen", "sim/betweenness-r0-n0.dat" u 1:4 axes x1y1 w lines lw 2 title "Infected - betweenness", "sim/betweenness-r0-n0.dat" u 1:5 axes x1y1 w lines lw 2 title "Recovered - betweenness", "sim/degree-r0-n0.dat" u 1:4 axes x1y1 w lines lw 2 title "Infected - degree", "sim/degree-r0-n0.dat" u 1:5 axes x1y1 w lines lw 2 title "Recovered - degree"
 # plot "sim/base-r0-n0.dat" u 1:4 axes x1y1 w lines lw 2 title "Infected", "sim/base-r0-n0.dat" u 1:5 axes x1y1 w lines lw 2 title "Recovered", "sim/eigen-r0-n0.dat" u 1:4 axes x1y1 w lines lw 2 title "Infected - eigen", "sim/eigen-r0-n0.dat" u 1:5 axes x1y1 w lines lw 2 title "Recovered - eigen", "sim/betweenness-r0-n0.dat" u 1:4 axes x1y1 w lines lw 2 title "Infected - betweenness", "sim/betweenness-r0-n0.dat" u 1:5 axes x1y1 w lines lw 2 title "Recovered - betweenness", "sim/population-r0-n0.dat" u 1:4 axes x1y1 w lines lw 2 title "Infected - population", "sim/population-r0-n0.dat" u 1:5 axes x1y1 w lines lw 2 title "Recovered - population"
 
-# plot
-# "sim/base-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered r0-n0",
-# "sim/base-r0-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered r0-n1",
-# "sim/base-r0-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered r0-n2",
-# "sim/base-r1-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered r1-n0",
-# "sim/base-r1-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered r1-n1",
-# "sim/base-r1-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered r1-n2",
-# "sim/base-r2-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered r2-n0",
-# "sim/base-r2-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered r2-n1",
-# "sim/base-r2-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered r2-n2"
+# Base
+# plot "sim/base-r0-n0.dat" u 1: 2 axes x1y1 w lines lw 2 title "Susceptible", "sim/base-r0-n0.dat" u 1: 3 axes x1y1 w lines lw 2 title "Exposed", "sim/base-r0-n0.dat" u 1: 4 axes x1y1 w lines lw 2 title "Infected", "sim/base-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 title "Recovered"
+# plot "sim/base-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered Upper", "sim/base-r0-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" notitle, "sim/base-r0-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" notitle, "sim/base-r1-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered Median", "sim/base-r1-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" notitle, "sim/base-r1-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" notitle, "sim/base-r2-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered Lower", "sim/base-r2-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" notitle, "sim/base-r2-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" notitle
+
+# Eigenvector
+# plot "sim/eigen-r0-n0.dat" u 1: 2 axes x1y1 w lines lw 2 title "Susceptible", "sim/eigen-r0-n0.dat" u 1: 3 axes x1y1 w lines lw 2 title "Exposed", "sim/eigen-r0-n0.dat" u 1: 4 axes x1y1 w lines lw 2 title "Infected", "sim/eigen-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 title "Recovered"
+# plot "sim/eigen-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered Upper", "sim/eigen-r0-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" notitle, "sim/eigen-r0-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" notitle, "sim/eigen-r1-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered Median", "sim/eigen-r1-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" notitle, "sim/eigen-r1-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" notitle, "sim/eigen-r2-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered Lower", "sim/eigen-r2-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" notitle, "sim/eigen-r2-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" notitle
+
+# Betweenness
+# plot "sim/betweenness-r0-n0.dat" u 1: 2 axes x1y1 w lines lw 2 title "Susceptible", "sim/betweenness-r0-n0.dat" u 1: 3 axes x1y1 w lines lw 2 title "Exposed", "sim/betweenness-r0-n0.dat" u 1: 4 axes x1y1 w lines lw 2 title "Infected", "sim/betweenness-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 title "Recovered"
+# plot "sim/betweenness-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered Upper", "sim/betweenness-r0-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" notitle, "sim/betweenness-r0-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" notitle, "sim/betweenness-r1-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered Median", "sim/betweenness-r1-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" notitle, "sim/betweenness-r1-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" notitle, "sim/betweenness-r2-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered Lower", "sim/betweenness-r2-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" notitle, "sim/betweenness-r2-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" notitle
+
+# Population
+# plot "sim/population-r0-n0.dat" u 1: 2 axes x1y1 w lines lw 2 title "Susceptible", "sim/population-r0-n0.dat" u 1: 3 axes x1y1 w lines lw 2 title "Exposed", "sim/population-r0-n0.dat" u 1: 4 axes x1y1 w lines lw 2 title "Infected", "sim/population-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 title "Recovered"
+# plot "sim/population-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered Upper", "sim/population-r0-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" notitle, "sim/population-r0-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" notitle, "sim/population-r1-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered Median", "sim/population-r1-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" notitle, "sim/population-r1-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" notitle, "sim/population-r2-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered Lower", "sim/population-r2-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" notitle, "sim/population-r2-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" notitle
+
+# Comparing
+# susceptible
+# plot "sim/base-r0-n0.dat" u 1: 2 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Base", "sim/eigen-r0-n0.dat" u 1: 2 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Eigenvector centrality", "sim/betweenness-r0-n0.dat" u 1: 2 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Betweenness centrality", "sim/population-r0-n0.dat" u 1: 2 axes x1y1 w lines lw 2 lt rgb "#3436BF" title "Population"
+
+# exposed
+# plot  "sim/base-r0-n0.dat" u 1: 3 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Base", "sim/eigen-r0-n0.dat" u 1: 3 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Eigenvector centrality", "sim/betweenness-r0-n0.dat" u 1: 3 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Betweenness centrality", "sim/population-r0-n0.dat" u 1: 3 axes x1y1 w lines lw 2 lt rgb "#3436BF" title "Population"
+
+# infected
+# plot "sim/base-r0-n0.dat" u 1: 4 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Base", "sim/eigen-r0-n0.dat" u 1: 4 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Eigenvector centrality", "sim/betweenness-r0-n0.dat" u 1: 4 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Betweenness centrality", "sim/population-r0-n0.dat" u 1: 4 axes x1y1 w lines lw 2 lt rgb "#3436BF" title "Population"
+
+# population
+# plot "sim/base-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Base", "sim/eigen-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Eigenvector centrality", "sim/betweenness-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Betweenness centrality", "sim/population-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#3436BF" title "Population"
 
 # plot "sim/population-r0-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered r0-n0","sim/population-r0-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered r0-n1","sim/population-r0-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C74931" title "Recovered r0-n2","sim/population-r1-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered r1-n0","sim/population-r1-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered r1-n1","sim/population-r1-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#C7C661" title "Recovered r1-n2","sim/population-r2-n0.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered r2-n0","sim/population-r2-n1.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered r2-n1","sim/population-r2-n2.dat" u 1: 5 axes x1y1 w lines lw 2 lt rgb "#9ACC8E" title "Recovered r2-n2"
